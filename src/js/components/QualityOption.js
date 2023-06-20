ccfd97911e90eb01966194da749279022ab2c710
dennyliudongqi <dennis.liudongqi@qq.com>
2023-06-20 17:34:52 +0800

    merge

--- src/js/components/QualityOption.js
+++ src/js/components/QualityOption.js
@@ -1,44 +1,47 @@
-var _ = require('underscore'),
-    events = require('../events');
+var _ = require("underscore"),
+  events = require("../events");
 
-module.exports = function(videojs) {
-   var MenuItem = videojs.getComponent('MenuItem');
+module.exports = function (videojs) {
+  var MenuItem = videojs.getComponent("MenuItem");
 
-   /**
-    * A MenuItem to represent a video resolution
-    *
-    * @class QualityOption
-    * @extends videojs.MenuItem
-    */
-   return videojs.extend(MenuItem, {
+  /**
+   * A MenuItem to represent a video resolution
+   *
+   * @class QualityOption
+   * @extends videojs.MenuItem
+   */
+  return class QualityOption extends MenuItem {
+    /**
+     * @inheritdoc
+     */
+    constructor(player, options) {
+      var source = options.source;
 
-      /**
-       * @inheritdoc
-       */
-      constructor: function(player, options) {
-         var source = options.source;
+      if (!_.isObject(source)) {
+        throw new Error(
+          'was not provided a "source" object, but rather: ' + typeof source
+        );
+      }
 
-         if (!_.isObject(source)) {
-            throw new Error('was not provided a "source" object, but rather: ' + (typeof source));
-         }
+      options = _.extend(
+        {
+          selectable: true,
+          label: source.label,
+        },
+        options
+      );
 
-         options = _.extend({
-            selectable: true,
-            label: source.label,
-         }, options);
+      super(player, options);
 
-         MenuItem.call(this, player, options);
+      this.source = source;
+    }
 
-         this.source = source;
-      },
-
-      /**
-       * @inheritdoc
-       */
-      handleClick: function(event) {
-         MenuItem.prototype.handleClick.call(this, event);
-         this.player().trigger(events.QUALITY_REQUESTED, this.source);
-      },
-
-   });
+    /**
+     * @inheritdoc
+     */
+    handleClick(event) {
+      super.handleClick(event);
+      this.player().trigger(events.QUALITY_REQUESTED, this.source);
+    }
+  };
 };
